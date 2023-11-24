a14c8c796afcf9b185f7e2b3fe101f2ef1d8f4bf
Tawonga021 <reginaldjongwe@gmail.com>
2023-11-24 02:41:59 -0800

    updated versions of manager,admin and cashier auth

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ function App() {
     
       <Routes>
        
-          <Route path="Mainheader" element={< Mainheader/>} />
-          <Route path="WelcomeAdmin" element={<WelcomeAdmin/>} />
-          <Route path="WelcomeManager" element={<WelcomeManager/>} />
-          <Route path="SighnUp" element={<SighnUp/>} />
-          <Route path="ManagerMain" element={<ManagerMain/>} />
-          <Route  path="Welcome" element={<Welcome/>}/>
+          <Route path="/Mainheader" element={< Mainheader/>} />
+          <Route path="/WelcomeAdmin" element={<WelcomeAdmin/>} />
+          <Route path="/WelcomeManager" element={<WelcomeManager/>} />
+          <Route path="/SighnUp" element={<SighnUp/>} />
+          <Route path="/ManagerMain" element={<ManagerMain/>} />
+          <Route  path="/Welcome" element={<Welcome/>}/>
               
       </Routes>
     </BrowserRouter>
